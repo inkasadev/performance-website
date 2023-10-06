4e58480d5f96a93bebc6351128adf6d3666bcb0e
Phillipe Martins <hi@inkasa.dev>
2023-10-05 22:37:46 -0300

    perf: next Image component test

--- src/components/BlogPostContent/BlogPostContent.tsx
+++ src/components/BlogPostContent/BlogPostContent.tsx
@@ -1,4 +1,5 @@
-import { ContentTransformer, Image } from "@crystallize/reactjs-components";
+import { ContentTransformer } from "@crystallize/reactjs-components";
+import Image from "next/image";
 import React from "react";
 
 interface IBlogPostContentProps {
@@ -15,11 +16,14 @@ export const BlogPostContent = ({ data }: IBlogPostContentProps) => {
           <div key={paragraphId}>
             <h2>{paragraph.title?.text}</h2>
             {paragraph.images?.map((image: any, contentId: number) => {
+              console.log("image => ", image);
               return (
                 paragraphId !== 0 && (
                   <Image
                     key={contentId}
-                    {...image}
+                    src={image.url}
+                    width={image.variants[image.variants.length - 1].width}
+                    height={image.variants[image.variants.length - 1].height}
                     alt={`${paragraph.title?.text || "blog post"} image`}
                     loading="lazy"
                   />
